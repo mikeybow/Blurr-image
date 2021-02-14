c9f4ae9dc666a9e5331d3b1d630602cc7ee40bc8
Michael Bowman <micbowman25@gmail.com>
2021-02-13 18:38:06 -0600

    finished css

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
-const scale = (num, in_min, in_max, out_min, out_max) => {
-    return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
- };
+// const scale = (num, in_min, in_max, out_min, out_max) => {
+//     return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+//  };

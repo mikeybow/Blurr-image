841823555c966498a82c14ffb7e7b7d8a4124c85
Michael Bowman <micbowman25@gmail.com>
2021-02-17 17:15:29 -0600

    finished project

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 const numb = document.querySelector(".loading-text");
-let counter = 0;
+const bg = document.querySelector(".bg");
 
-setInterval(() => {
-    if(counter === 100) {
-    clearInterval();
-    } else {
-        counter+=1;
-        numb.textContent = counter + "%";
+let load = 0;
+
+const bluring = () => {
+    load++;
+
+    if (load > 99) {
+        clearInterval(int)
     }
-}, 30);
+
+    numb.innerText = `${load}%`;
+    numb.style.opacity = scale(load, 0, 100, 1, 0);
+
+    bg.style.filter = `blur(${scale(load , 0, 100, 20, 0)}px)`
+};
 
 const scale = (num, in_min, in_max, out_min, out_max) => {
     return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
  };
+
+ const int = setInterval(bluring, 20);

a4e05465bae93f6636731ba1f7bfe525820798b8
Michael Bowman <micbowman25@gmail.com>
2021-02-17 16:40:53 -0600

    finished the progress counter

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
-// const scale = (num, in_min, in_max, out_min, out_max) => {
-//     return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
-//  };
+const numb = document.querySelector(".loading-text");
+let counter = 0;
+
+setInterval(() => {
+    if(counter === 100) {
+    clearInterval();
+    } else {
+        counter+=1;
+        numb.textContent = counter + "%";
+    }
+}, 30);
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+ };
